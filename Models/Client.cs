a05a0729d170f4df0b498b26caf198e2f16bbbef
Michael Carroll <128497239+mcarroll138@users.noreply.github.com>
2023-07-28 08:51:48 -0700

    add Client.cs Model

---
+++ Models/Client.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace Client.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public string ClientName { get; set; }
+    public int StylistId { get; set; }
+
+  }
+
+}
