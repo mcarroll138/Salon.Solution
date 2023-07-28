881bf1d9cb55f0525ffdf37a2725ea79e38e683b
Michael Carroll <128497239+mcarroll138@users.noreply.github.com>
2023-07-28 10:59:01 -0700

    debug csproj file, update framework Property Group and Item Group.

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -6,12 +6,31 @@ using System.Linq;
 
 namespace Salon.Controllers
 {
-  public class SylistsController : Controller
+  public class StylistsController : Controller
   {
     private readonly SalonContext _db;
     public StylistsController(SalonContext db)
     {
       _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Stylist stylist)
+    {
+      _db.Stylists.Add(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
