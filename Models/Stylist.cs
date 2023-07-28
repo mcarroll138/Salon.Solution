f82dab1dc80c55f9f01cd987ae17312bef741a4c
Michael Carroll <128497239+mcarroll138@users.noreply.github.com>
2023-07-28 08:47:24 -0700

    add Stylist.cs Model

---
+++ Models/Stylist.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Salon.Models
+{
+  public class Sylist
+  {
+    public int StylistId { get; set; }
+    public string StylistName { get; set; }
+
+  }
+}
